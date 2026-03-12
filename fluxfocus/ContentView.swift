be61f2ba82a57ba294afc68e52827abd35be6fbe
Alcu1n <alcuin.ch@gmail.com>
2026-03-12 21:38:10 +0800

    Initial Commit

---
+++ fluxfocus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fluxfocus
+//
+//  Created by 张慕坤 on 2026/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fluxfocus/fluxfocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  fluxfocusApp.swift
+//  fluxfocus
+//
+//  Created by 张慕坤 on 2026/3/12.
+//
+
+import SwiftUI
+
+@main
+struct fluxfocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
